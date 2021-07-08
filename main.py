2fa42d3c5d672f1caa5d5cfbb39dfcf7b3bb96b6
Eloy Chang <echangca@directvla.com.ar>
2021-07-08 12:23:41 -0300

    created test to push and test to PR to dev

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+from sys import argv
 
 print("Hello World!")
 
-#print("Generating error")
-#print(1/0)
-
-print("Error is fixed")
+if argv[1] != "error":
+    print(f"This is a {argv[1]} test")
+else:
+    print("Generating error")
+    print(1/0)
